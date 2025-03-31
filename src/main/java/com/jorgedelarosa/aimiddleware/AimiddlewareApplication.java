4c045ff07da7f5d97af08d828d8f83280d278c19
Jorge <jorgedlrg@gmail.com>
2025-03-31 18:43:57 +0200

    add spring boot

---
+++ src/main/java/com/jorgedelarosa/aimiddleware/AimiddlewareApplication.java
@@ -0,0 +1,13 @@
+package com.jorgedelarosa.aimiddleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AimiddlewareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AimiddlewareApplication.class, args);
+	}
+
+}
