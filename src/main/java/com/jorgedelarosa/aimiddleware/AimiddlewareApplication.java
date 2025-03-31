086b2002ba725445dfd98186ae2f0385d0a52959
Jorge <jorgedlrg@gmail.com>
2025-03-31 18:55:09 +0200

    format

--- src/main/java/com/jorgedelarosa/aimiddleware/AimiddlewareApplication.java
+++ src/main/java/com/jorgedelarosa/aimiddleware/AimiddlewareApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AimiddlewareApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AimiddlewareApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AimiddlewareApplication.class, args);
+    }
 
 }
