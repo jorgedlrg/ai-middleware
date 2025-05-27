f13fcd4885142cbaf85d2f81f70b358699f84590
Jorge <jorgedlrg@gmail.com>
2025-05-27 11:12:26 +0200

    :construction: create tables

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,12 @@
  * Created: 22 may 2025
  */
 
+/** TODO: Add audit data **/
+
+CREATE TABLE scenario (id UUID);
+INSERT INTO scenario VALUES ("7376f89d-4ca7-423b-95f1-e29a8832ec4a");
+
+CREATE TABLE session (id UUID, scenario UUID);
+INSERT INTO scenario VALUES ("7376f89d-4ca7-423b-95f1-e29a8832ec4a","7376f89d-4ca7-423b-95f1-e29a8832ec4a");
+
+CREATE TABLE interaction (id UUID, role UUID, session UUID, timestamp INT, text LONGTEXT);
