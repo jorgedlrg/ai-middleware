6853bf5484a3b23254746af33c53717d98a82497
Jorge <jorgedlrg@gmail.com>
2025-06-18 14:03:02 +0200

    :construction: use messageItemList for interactions

--- src/main/java/com/jorgedelarosa/aimiddleware/adapter/in/ui/InteractionView.java
+++ src/main/java/com/jorgedelarosa/aimiddleware/adapter/in/ui/InteractionView.java
@@ -4,8 +4,8 @@ import com.jorgedelarosa.aimiddleware.application.port.in.MachineInteractUseCase
 import com.jorgedelarosa.aimiddleware.application.port.in.RetrieveSessionInteractionsUseCase;
 import com.jorgedelarosa.aimiddleware.application.port.in.UserInteractUseCase;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.grid.GridVariant;
+import com.vaadin.flow.component.messages.MessageList;
+import com.vaadin.flow.component.messages.MessageListItem;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextArea;
@@ -14,10 +14,10 @@ import com.vaadin.flow.router.BeforeEvent;
 import com.vaadin.flow.router.HasUrlParameter;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import java.util.ArrayList;
+import java.time.Instant;
 import java.util.UUID;
-
-// FIXME This is the first time I use Vaadin (I'm not a frontend DEV) so, take it easy.
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 /**
  * @author jorge
@@ -33,7 +33,7 @@ public class InteractionView extends VerticalLayout implements HasUrlParameter<S
 
   private final TextArea inputText = new TextArea();
 
-  private final Grid<RetrieveSessionInteractionsUseCase.InteractionDto> interactionGrid;
+  private final MessageList interationList;
 
   public InteractionView(
       UserInteractUseCase userInteractUseCase,
@@ -51,23 +51,9 @@ public class InteractionView extends VerticalLayout implements HasUrlParameter<S
     Button machineButton = new Button("Generate Machine Interaction");
     machineButton.addClickListener(e -> machineInteractListener());
 
-    interactionGrid = new Grid<>();
-    interactionGrid
-        .addColumn(RetrieveSessionInteractionsUseCase.InteractionDto::timestamp)
-        .setHeader("Timestamp")
-        .setFlexGrow(1);
-    interactionGrid
-        .addColumn(RetrieveSessionInteractionsUseCase.InteractionDto::actor)
-        .setHeader("Actor")
-        .setFlexGrow(2);
-    interactionGrid
-        .addColumn(RetrieveSessionInteractionsUseCase.InteractionDto::spokenText)
-        .setHeader("Text")
-        .setFlexGrow(7);
-    interactionGrid.setItems(new ArrayList<>());
-    interactionGrid.addThemeVariants(GridVariant.LUMO_WRAP_CELL_CONTENT);
+    interationList = new MessageList();
 
-    add(interactionGrid);
+    add(interationList);
     add(new HorizontalLayout(inputText, interactButton));
     add(machineButton);
   }
@@ -79,7 +65,7 @@ public class InteractionView extends VerticalLayout implements HasUrlParameter<S
             UUID.fromString("7376f89d-4ca7-423b-95f1-e29a8832ec4a"),
             inputText.getValue()));
 
-    fillInteractionGrid();
+    fillInteractionList();
   }
 
   private void machineInteractListener() {
@@ -87,18 +73,34 @@ public class InteractionView extends VerticalLayout implements HasUrlParameter<S
         new MachineInteractUseCase.Command(
             session, UUID.fromString("655cfb3d-c740-48d2-ab4f-51e391c4deaf")));
 
-    fillInteractionGrid();
+    fillInteractionList();
   }
 
-  private void fillInteractionGrid() {
-    interactionGrid.setItems(
-        retrieveSessionInteractionsUseCase.execute(
-            new RetrieveSessionInteractionsUseCase.Command(session)));
+  private void fillInteractionList() {
+    interationList.setItems(
+        retrieveSessionInteractionsUseCase
+            .execute(new RetrieveSessionInteractionsUseCase.Command(session))
+            .stream()
+            .map(e -> MessageMapper.INSTANCE.toMessage(e))
+            .toList());
   }
 
   @Override
   public void setParameter(BeforeEvent be, String t) {
     session = UUID.fromString(t);
-    fillInteractionGrid();
+    fillInteractionList();
+  }
+
+  @Mapper
+  public interface MessageMapper {
+    MessageMapper INSTANCE = Mappers.getMapper(MessageMapper.class);
+
+    default MessageListItem toMessage(RetrieveSessionInteractionsUseCase.InteractionDto dto) {
+      MessageListItem item =
+          new MessageListItem(dto.spokenText(), Instant.now(), dto.actor().toString());
+      // FIXME Max 5 different colors
+      item.setUserColorIndex(Math.abs(dto.actor().hashCode()) % 5);
+      return item;
+    }
   }
 }
