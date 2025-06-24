7e19d6680310d0d89b366a37dcdf598486c0b37a
Jorge <jorgedlrg@gmail.com>
2025-06-24 13:03:36 +0200

    :construction: UI for answer language

--- src/main/java/com/jorgedelarosa/aimiddleware/adapter/in/ui/SessionView.java
+++ src/main/java/com/jorgedelarosa/aimiddleware/adapter/in/ui/SessionView.java
@@ -2,8 +2,10 @@ package com.jorgedelarosa.aimiddleware.adapter.in.ui;
 
 import com.jorgedelarosa.aimiddleware.application.port.in.MachineInteractUseCase;
 import com.jorgedelarosa.aimiddleware.application.port.in.RetrieveSessionInteractionsUseCase;
+import com.jorgedelarosa.aimiddleware.application.port.in.UpdateSessionUseCase;
 import com.jorgedelarosa.aimiddleware.application.port.in.UserInteractUseCase;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.messages.MessageInput;
 import com.vaadin.flow.component.messages.MessageList;
 import com.vaadin.flow.component.messages.MessageListItem;
@@ -12,7 +14,7 @@ import com.vaadin.flow.router.BeforeEvent;
 import com.vaadin.flow.router.HasDynamicTitle;
 import com.vaadin.flow.router.HasUrlParameter;
 import com.vaadin.flow.router.Route;
-import java.time.Instant;
+import java.util.Locale;
 import java.util.UUID;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
@@ -26,18 +28,22 @@ public class SessionView extends VerticalLayout
   private final UserInteractUseCase userInteractUseCase;
   private final MachineInteractUseCase machineInteractUseCase;
   private final RetrieveSessionInteractionsUseCase retrieveSessionInteractionsUseCase;
+  private final UpdateSessionUseCase updateSessionUseCase;
 
   private UUID session;
   private String pageTitle;
   private final MessageList interationList;
+  private ComboBox<Locale> localeComboBox;
 
   public SessionView(
       UserInteractUseCase userInteractUseCase,
       MachineInteractUseCase machineInteractUseCase,
-      RetrieveSessionInteractionsUseCase retrieveSessionInteractionsUseCase) {
+      RetrieveSessionInteractionsUseCase retrieveSessionInteractionsUseCase,
+      UpdateSessionUseCase updateSessionUseCase) {
     this.userInteractUseCase = userInteractUseCase;
     this.machineInteractUseCase = machineInteractUseCase;
     this.retrieveSessionInteractionsUseCase = retrieveSessionInteractionsUseCase;
+    this.updateSessionUseCase = updateSessionUseCase;
 
     MessageInput input =
         new MessageInput(
@@ -51,9 +57,19 @@ public class SessionView extends VerticalLayout
 
     interationList = new MessageList();
 
+    localeComboBox = new ComboBox<>("Answer language");
+    localeComboBox.setItems(Locale.ENGLISH, Locale.CHINESE, Locale.forLanguageTag("es"));
+    localeComboBox.setItemLabelGenerator(Locale::getDisplayLanguage);
+    localeComboBox.setValue(
+        Locale.forLanguageTag(
+            "es")); // TODO: instead of 'retrieve interactions', retrieve the full session and REDO
+    // this view
+    localeComboBox.addValueChangeListener(e -> changeLocaleListener(e.getValue()));
+
     add(interationList);
     add(input);
     add(machineButton);
+    add(localeComboBox);
   }
 
   private void userInteractListener(MessageInput.SubmitEvent submitEvent) {
@@ -72,6 +88,10 @@ public class SessionView extends VerticalLayout
             session, UUID.fromString("655cfb3d-c740-48d2-ab4f-51e391c4deaf")));
 
     fillInteractionList();
+  }
+
+  private void changeLocaleListener(Locale locale) {
+    updateSessionUseCase.execute(new UpdateSessionUseCase.Command(session, locale));
   }
 
   private void fillInteractionList() {
@@ -107,9 +127,9 @@ public class SessionView extends VerticalLayout
 
     default MessageListItem toMessage(RetrieveSessionInteractionsUseCase.InteractionDto dto) {
       MessageListItem item =
-          new MessageListItem(dto.spokenText(), Instant.now(), dto.actor().toString());
+          new MessageListItem(dto.spokenText(), dto.timestamp(), dto.actorName());
       // FIXME Max 5 different colors
-      item.setUserColorIndex(Math.abs(dto.actor().hashCode()) % 5);
+      item.setUserColorIndex(Math.abs(dto.actorName().hashCode()) % 5);
       return item;
     }
   }
--- src/main/java/com/jorgedelarosa/aimiddleware/application/port/in/RetrieveSessionInteractionsUseCaseImpl.java
+++ src/main/java/com/jorgedelarosa/aimiddleware/application/port/in/RetrieveSessionInteractionsUseCaseImpl.java
@@ -1,9 +1,12 @@
 package com.jorgedelarosa.aimiddleware.application.port.in;
 
+import com.jorgedelarosa.aimiddleware.application.port.out.GetActorByIdOutPort;
 import com.jorgedelarosa.aimiddleware.application.port.out.GetSessionByIdOutPort;
+import com.jorgedelarosa.aimiddleware.domain.actor.Actor;
 import com.jorgedelarosa.aimiddleware.domain.session.Interaction;
 import com.jorgedelarosa.aimiddleware.domain.session.Session;
 import java.util.List;
+import java.util.Optional;
 import lombok.AllArgsConstructor;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
@@ -17,13 +20,14 @@ import org.springframework.stereotype.Component;
 public class RetrieveSessionInteractionsUseCaseImpl implements RetrieveSessionInteractionsUseCase {
 
   private final GetSessionByIdOutPort getSessionByIdOutPort;
+  private final GetActorByIdOutPort getActorByIdOutPort;
 
   @Override
   public List<InteractionDto> execute(Command cmd) {
     Session session = getSessionByIdOutPort.query(cmd.session()).orElseThrow();
 
     return session.getInteractions().stream()
-        .map((e) -> InteractionMapper.INSTANCE.toDto(e))
+        .map((e) -> InteractionMapper.INSTANCE.toDto(e, getActorByIdOutPort.query(e.getActor())))
         .toList();
   }
 
@@ -31,6 +35,9 @@ public class RetrieveSessionInteractionsUseCaseImpl implements RetrieveSessionIn
   public interface InteractionMapper {
     InteractionMapper INSTANCE = Mappers.getMapper(InteractionMapper.class);
 
-    InteractionDto toDto(Interaction dom);
+    default InteractionDto toDto(Interaction dom, Optional<Actor> actor) {
+      return new InteractionDto(
+          dom.getTimestamp(), actor.orElseThrow().getName(), dom.getSpokenText());
+    }
   }
 }
