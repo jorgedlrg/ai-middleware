9712edbe27a910c7c30970241354d3d12a4ff1ca
Jorge <jorgedlrg@gmail.com>
2025-07-30 12:09:35 +0200

    :construction: Save & Delete Introduction

--- src/main/java/com/jorgedelarosa/aimiddleware/adapter/in/ui/IntroductionEditorView.java
+++ src/main/java/com/jorgedelarosa/aimiddleware/adapter/in/ui/IntroductionEditorView.java
@@ -1,11 +1,14 @@
 package com.jorgedelarosa.aimiddleware.adapter.in.ui;
 
 import com.jorgedelarosa.aimiddleware.adapter.in.ui.components.DeleteConfirmButton;
+import com.jorgedelarosa.aimiddleware.application.port.in.scenario.DeleteIntroductionUseCase;
 import com.jorgedelarosa.aimiddleware.application.port.in.scenario.GetScenarioDetailsUseCase;
+import com.jorgedelarosa.aimiddleware.application.port.in.scenario.SaveIntroductionUseCase;
 import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.confirmdialog.ConfirmDialog;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.Div;
@@ -30,6 +33,8 @@ public class IntroductionEditorView extends VerticalLayout
     implements BeforeEnterObserver, HasDynamicTitle {
 
   private final GetScenarioDetailsUseCase getScenarioDetailsUseCase;
+  private final SaveIntroductionUseCase saveIntroductionUseCase;
+  private final DeleteIntroductionUseCase deleteIntroductionUseCase;
 
   private String pageTitle;
 
@@ -41,22 +46,44 @@ public class IntroductionEditorView extends VerticalLayout
   private TextArea spoken;
   private TextArea thoughts;
   private TextArea action;
+  private ComboBox<GetScenarioDetailsUseCase.RoleDto> roles;
+  private ComboBox<GetScenarioDetailsUseCase.ContextDto> contexts;
 
   private void render() {
     removeAll();
     spoken = new TextArea("Spoken text");
     thoughts = new TextArea("Thoughts");
     action = new TextArea("Action");
+    roles = new ComboBox<>("Performer");
+    roles.setItems(scenarioDto.roles());
+    roles.setItemLabelGenerator(GetScenarioDetailsUseCase.RoleDto::name);
+    roles.setRequiredIndicatorVisible(true);
+    contexts = new ComboBox<>("Context");
+    contexts.setItems(scenarioDto.contexts());
+    contexts.setItemLabelGenerator(GetScenarioDetailsUseCase.ContextDto::name);
+    contexts.setRequiredIndicatorVisible(true);
     if (dto != null) {
       spoken.setValue(dto.spokenText());
       thoughts.setValue(dto.thoughtText().orElse(""));
       action.setValue(dto.actionText().orElse(""));
+      roles.setValue(
+          scenarioDto.roles().stream()
+              .filter(e -> e.id().equals(dto.performer()))
+              .findFirst()
+              .orElseThrow());
+      contexts.setValue(
+          scenarioDto.contexts().stream()
+              .filter(e -> e.id().equals(dto.context()))
+              .findFirst()
+              .orElseThrow());
     }
-    
+
     FormLayout formLayout = new FormLayout();
     formLayout.add(thoughts, 3);
     formLayout.add(action, 3);
     formLayout.add(spoken, 3);
+    formLayout.add(roles, 1);
+    formLayout.add(contexts, 1);
 
     Button saveButton = new Button("Save");
     saveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
@@ -68,10 +95,26 @@ public class IntroductionEditorView extends VerticalLayout
     add(formLayout);
     add(new Div(saveButton, deleteButton));
   }
-  
-    private ComponentEventListener<ClickEvent<Button>> saveIntroListener() {
+
+  private ComponentEventListener<ClickEvent<Button>> saveIntroListener() {
     return (ClickEvent<Button> t) -> {
- // TODO call use case
+      String sth = null;
+      if (!thoughts.getValue().equals("")) {
+        sth = thoughts.getValue();
+      }
+      String sact = null;
+      if (!action.getValue().equals("")) {
+        sact = action.getValue();
+      }
+      saveIntroductionUseCase.execute(
+          new SaveIntroductionUseCase.Command(
+              scenario,
+              introduction,
+              spoken.getValue(),
+              Optional.ofNullable(sth),
+              Optional.ofNullable(sact),
+              roles.getValue().id(),
+              contexts.getValue().id()));
       t.getSource().getUI().ifPresent(ui -> ui.navigate("scenarios/" + scenario));
       Notification notification = Notification.show("Introduction saved!");
       notification.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
@@ -80,7 +123,8 @@ public class IntroductionEditorView extends VerticalLayout
 
   private ComponentEventListener<ConfirmDialog.ConfirmEvent> deleteIntroListener() {
     return (ConfirmDialog.ConfirmEvent t) -> {
-      // TODO call use case
+      deleteIntroductionUseCase.execute(
+          new DeleteIntroductionUseCase.Command(scenario, introduction));
       t.getSource().getUI().ifPresent(ui -> ui.navigate("scenarios/" + scenario));
       Notification notification = Notification.show("Introduction deleted!");
       notification.addThemeVariants(NotificationVariant.LUMO_PRIMARY);
@@ -93,8 +137,8 @@ public class IntroductionEditorView extends VerticalLayout
     scenarioDto =
         getScenarioDetailsUseCase.execute(new GetScenarioDetailsUseCase.Command(scenario));
 
-    Optional<String> sc = event.getRouteParameters().get("introductionId");
-    sc.ifPresent(e -> introduction = UUID.fromString(e));
+    introduction =
+        event.getRouteParameters().get("introductionId").map(e -> UUID.fromString(e)).orElse(null);
     if (introduction != null) {
       dto =
           scenarioDto.introductions().stream()
