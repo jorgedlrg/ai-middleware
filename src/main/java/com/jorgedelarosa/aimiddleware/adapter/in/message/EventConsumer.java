3aaaffa376a9215536b16713ef668c340f589580
Jorge <jorgedlrg@gmail.com>
2025-08-05 10:48:59 +0200

    :wrench: rename EventConsumer

--- src/main/java/com/jorgedelarosa/aimiddleware/adapter/in/message/EventConsumer.java
+++ src/main/java/com/jorgedelarosa/aimiddleware/adapter/in/message/EventConsumer.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public class OutboxEventConsumer {
+public class EventConsumer {
 
   private final RemoveOutfitAllActorsUseCase removeOutfitAllActorsUseCase;
 
