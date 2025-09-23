0e90146d7c9c644f2c3759b1c6200e161b0f328d
Jorge <jorgedlrg@gmail.com>
2025-09-23 18:16:13 +0200

    :construction: code quality & bump dependencies

--- src/main/java/com/jorgedelarosa/aimiddleware/adapter/in/message/EventConsumer.java
+++ src/main/java/com/jorgedelarosa/aimiddleware/adapter/in/message/EventConsumer.java
@@ -4,6 +4,10 @@ import com.jorgedelarosa.aimiddleware.adapter.out.message.EventEnvelope;
 import com.jorgedelarosa.aimiddleware.application.port.in.actor.RemoveOutfitAllActorsUseCase;
 import com.jorgedelarosa.aimiddleware.application.port.in.session.DeleteSessionWithActorUseCase;
 import com.jorgedelarosa.aimiddleware.application.port.in.session.DeleteSessionWithScenarioUseCase;
+import com.jorgedelarosa.aimiddleware.domain.DomainEvent;
+import com.jorgedelarosa.aimiddleware.domain.actor.ActorDeletedEvent;
+import com.jorgedelarosa.aimiddleware.domain.actor.OutfitDeletedEvent;
+import com.jorgedelarosa.aimiddleware.domain.scenario.ScenarioDeletedEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
@@ -17,31 +21,32 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class EventConsumer {
 
-  private final DeleteSessionWithActorUseCase deleteSessionWithActorUseCase;
-  private final DeleteSessionWithScenarioUseCase deleteSessionWithScenarioUseCase;
-  private final RemoveOutfitAllActorsUseCase removeOutfitAllActorsUseCase;
+    private final DeleteSessionWithActorUseCase deleteSessionWithActorUseCase;
+    private final DeleteSessionWithScenarioUseCase deleteSessionWithScenarioUseCase;
+    private final RemoveOutfitAllActorsUseCase removeOutfitAllActorsUseCase;
 
-  @EventListener
-  public void handleMessage(EventEnvelope envelope) {
-    switch (envelope.getEventType()) {
-      case "com.jorgedelarosa.aimiddleware.domain.actor.ActorDeletedEvent" -> {
-        log.info("Handling com.jorgedelarosa.aimiddleware.domain.actor.ActorDeletedEvent");
-        deleteSessionWithActorUseCase.execute(
-            new DeleteSessionWithActorUseCase.Command(
-                envelope.getEvent().getAggregateId().getId()));
-      }
-      case "com.jorgedelarosa.aimiddleware.domain.actor.OutfitDeletedEvent" -> {
-        log.info("Handling com.jorgedelarosa.aimiddleware.domain.actor.OutfitDeletedEvent");
-        removeOutfitAllActorsUseCase.execute(
-            new RemoveOutfitAllActorsUseCase.Command(envelope.getEvent().getAggregateId().getId()));
-      }
-      case "com.jorgedelarosa.aimiddleware.domain.scenario.ScenarioDeletedEvent" -> {
-        log.info("Handling com.jorgedelarosa.aimiddleware.domain.scenario.ScenarioDeletedEvent");
-        deleteSessionWithScenarioUseCase.execute(
-            new DeleteSessionWithScenarioUseCase.Command(
-                envelope.getEvent().getAggregateId().getId()));
-      }
-      default -> log.info(String.format("Unhandled event type: %s", envelope.getEventType()));
+    @EventListener
+    public void handleMessage(EventEnvelope<? extends DomainEvent> envelope) {
+        switch (envelope.getEvent()) {
+            case ActorDeletedEvent event -> {
+                log.info("Handling ActorDeletedEvent");
+                deleteSessionWithActorUseCase.execute(
+                        new DeleteSessionWithActorUseCase.Command(
+                                event.getAggregateId().getId()));
+            }
+            case OutfitDeletedEvent event -> {
+                log.info("Handling OutfitDeletedEvent");
+                removeOutfitAllActorsUseCase.execute(
+                        new RemoveOutfitAllActorsUseCase.Command(event.getAggregateId().getId()));
+            }
+            case ScenarioDeletedEvent event -> {
+                log.info("Handling ScenarioDeletedEvent");
+                deleteSessionWithScenarioUseCase.execute(
+                        new DeleteSessionWithScenarioUseCase.Command(
+                                event.getAggregateId().getId()));
+            }
+            default ->
+                log.warn(String.format("Unhandled event type: %s", envelope.getEventType()));
+        }
     }
-  }
 }
