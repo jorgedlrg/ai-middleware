a180aff6bb6c13d733114196bf6c56d298276050
Jorge <jorgedlrg@gmail.com>
2025-09-30 19:14:52 +0200

    :white_check_mark: InteractionText tests & fix

--- src/main/java/com/jorgedelarosa/aimiddleware/domain/session/InteractionText.java
+++ src/main/java/com/jorgedelarosa/aimiddleware/domain/session/InteractionText.java
@@ -6,27 +6,31 @@ import java.util.Optional;
  * @author jorge
  */
 public class InteractionText {
-  private final String text;
-  private final Optional<String> reasoning;
 
-  public static Optional<InteractionText> optionalFromNullable(String text, String reasoning) {
-    if (text != null) {
-      return Optional.of(new InteractionText(text, Optional.ofNullable(reasoning)));
-    } else {
-      return Optional.empty();
+    private final String text;
+    private final Optional<String> reasoning;
+
+    public static Optional<InteractionText> optionalFromNullable(String text, String reasoning) {
+        if (text != null) {
+            return Optional.of(new InteractionText(text, Optional.ofNullable(reasoning)));
+        } else {
+            return Optional.empty();
+        }
     }
-  }
 
-  public InteractionText(String text, Optional<String> reasoning) {
-    this.text = text.trim();
-    this.reasoning = reasoning.map(e -> e.trim());
-  }
+    public InteractionText(String text, Optional<String> reasoning) {
+        this.text = text.trim();
+        if (text.isBlank()) {
+            throw new RuntimeException("Interaction text can't be blank");
+        }
+        this.reasoning = reasoning.map(e -> e.trim());
+    }
 
-  public String getText() {
-    return text;
-  }
+    public String getText() {
+        return text;
+    }
 
-  public Optional<String> getReasoning() {
-    return reasoning;
-  }
+    public Optional<String> getReasoning() {
+        return reasoning;
+    }
 }
